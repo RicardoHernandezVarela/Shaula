4588905e4cfe746b82633fcfeb15c80207faaf13
RicardoHernandezVarela <rhvarela011192@gmail.com>
2019-02-10 20:19:35 -0600

    Cambio de posgreSQLa sqlite

--- aula/settings.py
+++ aula/settings.py
@@ -76,6 +76,14 @@ WSGI_APPLICATION = 'aula.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shaula',
         'USER': 'postgres',
@@ -84,7 +92,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
