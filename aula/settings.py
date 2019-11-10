fc2884bd8b6706479567672c4f618260c8e7e76c
RicardoHernandezVarela <rhvarela011192@gmail.com>
2019-11-10 16:40:08 -0600

    Agregar calificaciones

--- aula/settings.py
+++ aula/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'aula.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,7 +92,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
